16fd505f8b3068b221504a84ba843554504ada15
Daniel Fernandez <Daniel.Fernandez@migcan.com>
2013-10-25 17:19:02 +0100

    WIP: Added multiplication grid class and prime spec.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,24 @@
 #!/usr/bin/env ruby
 
+class MultiplcationGrid
+
+  def self.output(array)
+    # print column headers
+    array.each {|x| print "\t#{x}" }
+
+    puts
+    # print rows
+    array.each do |x|
+      line = "#{x}\t"
+      array.each do |y|
+        line += "#{x * y}\t"
+      end
+      puts line
+    end
+  end
+
+end
+
 class Prime
 
   def self.sieve_up_to(n)
@@ -37,7 +56,7 @@ class Prime
     return true
   end
 
-  def self.first(amount_of_primes)
+  def self.take(amount_of_primes)
     primes = []
 
     number = 1
@@ -45,28 +64,18 @@ class Prime
       primes << number if self.is_prime?(number)
       number += 1
     end
+
+    return primes
   end
 
-  def self.print_grid_up_to(n)
-    primes = self.sieve_up_to(n)
-
-    # print column headers
-    primes.each {|x| print "\t#{x}" }
-
-    puts
-    # print rows
-    primes.each do |x|
-      line = "#{x}\t"
-      primes.each do |y|
-        line += "#{x * y}\t"
-      end
-      puts line
-    end
-  end
 end
 
-# def print_prime_grid(n)
-#   Prime.print_grid_up_to(n)
-# end
+def print_prime_grid(n)
+  primes = Prime.take(n)
+  MultiplcationGrid.output(primes)
+end
 
-# ARGV[0].to_i == 0 ? print_prime_grid(13) : print_prime_grid(ARGV[0].to_i)
+if $0 == __FILE__
+  ARGV[0].to_i == 0 ? print_prime_grid(10) : print_prime_grid(ARGV[0].to_i)
+end
+
