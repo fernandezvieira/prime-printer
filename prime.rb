0e209f3b0612a707dafd9779e501209960e1135f
Daniel Fernandez <Daniel.Fernandez@migcan.com>
2013-10-26 19:52:57 +0100

    Improved printing.

--- prime.rb
+++ prime.rb
@@ -2,12 +2,15 @@
 
 class MultiplcationGrid
 
-  def self.output(array)
+  def self.print(array)
     output_buffer = "       " + ((" %7d" * array.size) % array) + "\n"
 
-    array.each do |n|
-      output_buffer << "%7d" % n
-      array.each {|m| output_buffer << " %7d" % (n*m)}
+    array.each_with_index do |n, index|
+      line_spacing = "        "*index
+      output_buffer  << "%7d" % n << line_spacing
+      index.upto(array.size-1) do |i|
+        output_buffer << " %7d" % (n*array[i])
+      end
 
       output_buffer << "\n"
     end
@@ -45,12 +48,11 @@ class Prime
 
 end
 
-def print_prime_grid(n)
-  primes = Prime.take(n)
-  MultiplcationGrid.output(primes)
+def print_prime_grid(amount_of_primes)
+  primes = Prime.take(amount_of_primes)
+  MultiplcationGrid.print(primes)
 end
 
 if $0 == __FILE__
   ARGV[0].to_i == 0 ? print_prime_grid(10) : print_prime_grid(ARGV[0].to_i)
 end
-
