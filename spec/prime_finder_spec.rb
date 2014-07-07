68201b36ef230028b11378aa722139b56e894763
Daniel Fernandez <Daniel.Fernandez@migcan.com>
2014-07-07 09:19:57 +0100

    Add another spec to check that is prime.

--- spec/prime_finder_spec.rb
+++ spec/prime_finder_spec.rb
@@ -1,15 +1,26 @@
 require_relative '../lib/prime_finder'
 
+require 'prime'
+
 describe PrimeFinder do
   describe ".get_up_to" do
-    it "should return the correct amount of primes" do
+    it "returns primes" do
+      primes = PrimeFinder.get_up_to(5)
+
+      # use actual ruby library as we know this has been tested
+      primes.each do |prime|
+        expect(Prime.prime?(prime)).to be_true
+      end
+    end
+
+    it "returns the correct amount of primes" do
       PrimeFinder.get_up_to(5).size.should    == 5
       PrimeFinder.get_up_to(50).size.should   == 50
       PrimeFinder.get_up_to(500).size.should  == 500
       PrimeFinder.get_up_to(5000).size.should == 5000
     end
 
-    it "should return an empty array if there exists no primes" do
+    it "returns an empty array if there exists no primes" do
       PrimeFinder.get_up_to(0).should be_empty
     end
   end
